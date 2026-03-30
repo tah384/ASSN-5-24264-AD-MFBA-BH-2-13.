1c10616644ca96b746e93e503bc9e4e6b0d090ac
tah384 <115026575+tah384@users.noreply.github.com>
2026-03-30 14:49:15 +0500

    Create c6_q5.py

---
+++ c6_q5.py
@@ -0,0 +1,8 @@
+# Q5
+names = ["Ali", "Sara", "Ahmed", "Zara", "Usman"]
+name = input("Enter name to search: ")
+
+if name in names:
+    print(name, "is present in the list")
+else:
+    print(name, "is not in the list")
