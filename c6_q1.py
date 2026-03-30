5404ad0235f25d18175048dcac34c05dfd34f412
tah384 <115026575+tah384@users.noreply.github.com>
2026-03-30 14:47:45 +0500

    Update c6_q1.py

--- c6_q1.py
+++ c6_q1.py
@@ -3,3 +3,13 @@ n1 = float(input("Enter number 1: "))
 n2 = float(input("Enter number 2: "))
 n3 = float(input("Enter number 3: "))
 n4 = float(input("Enter number 4: "))
+
+greatest = n1
+if n2 > greatest:
+    greatest = n2
+if n3 > greatest:
+    greatest = n3
+if n4 > greatest:
+    greatest = n4
+
+print("Greatest:", greatest)
