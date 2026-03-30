e7aec20160ae0c78da8267e077c121f72573f971
tah384 <115026575+tah384@users.noreply.github.com>
2026-03-30 14:49:59 +0500

    Create c6_q6.py

---
+++ c6_q6.py
@@ -0,0 +1,15 @@
+# Q6
+marks = float(input("Enter marks: "))
+
+if marks >= 90 and marks <= 100:
+    print("Grade: Ex")
+elif marks >= 80:
+    print("Grade: A")
+elif marks >= 70:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+elif marks >= 50:
+    print("Grade: D")
+else:
+    print("Grade: F")
